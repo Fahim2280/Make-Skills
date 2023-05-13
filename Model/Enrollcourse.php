724222ce635ed23b4dc6ea65ca4f20af64bf2347
Md. Fahim Alam <kfahim2280@gmail.com>
2023-05-13 17:11:42 +0600

    Update Enrollcourse.php

--- Model/Enrollcourse.php
+++ Model/Enrollcourse.php
@@ -33,15 +33,3 @@ function deleteEnrollcourse($id)
         return false;
     }
 }
-
-function updateEnrollcourse($data)
-{
-    $conn = getConnection();
-    $sql = "update enrollcourse set Course_name='{$data['course_name']}',Course_code='{$data['course_code']}',Section='{$data['section']}',Teacher_name='{$data['teacher_name']}',Student_name='{$data['student_name']}' where ID='{$data['id']}'";
-    echo $sql;
-    if (mysqli_query($conn, $sql)) {
-        return true;
-    } else {
-        return false;
-    }
-}
